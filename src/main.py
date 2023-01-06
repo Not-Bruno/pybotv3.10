cb0dd89ef7187a71efd58dad881ec8a8bf832e08
Bruno <brunoscholz@gmx.de>
2023-01-06 12:00:23 +0100

    modified:   requirements.txt 	new file:   src/.env 	new file:   src/main.py

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO) # ?
+log = logging.getLogger('BOT-MAIN') # ?
+
+# Discord Bot definition
+bot = commands.Bot(
+    command_prefix=None, # Nur / commands
+    intents=discord.intents.default(), # ?
+    activity=discord.Activity(type=discord.ActivityType.watching, name="Discord"), # aktivität
+    status=discord.Status(type=discord.Status.online), # online status
+    sync_commands=False, # ?
+    delete_not_existing_commands=False, # ?
+)
+
+if __name__ == "__main__":
+    log.info("Starting Discord Bot ...")
+    token = os.getenv("BOT_TOKEN")
+    bot.run(token=token)
